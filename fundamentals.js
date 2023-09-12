8ab4645e625079f8f88cf3bc3f57baa619196f8c
Shaobo Zang <20105527@mail.wit.ie>
2023-09-13 05:48:38 +0800

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -7,11 +7,29 @@ const aCar = {
     },
     registration :{
                 year:201,
-                countycode:'WD',
+                countyCode:'WD',
                 number:1058
-    }  
+    } ,
+    mileage:10000,
+    colour:{
+        exteriorColour:'red',
+        interiorFabric:{
+            texture:'leather',
+            shade:'cream'
+        }
+    }
+
 };
   
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
 
-  console.log('Reg.='+aCar.registration.number+'-'+aCar.registration.countycode+'-'+aCar.registration.year)
+  console.log('Reg.='+aCar.registration.number+'-'+aCar.registration.countyCode+'-'+aCar.registration.year)
+
+  console.log(
+    'It is a '+
+    aCar.colour.exteriorColour+
+    ' car,'+
+    aCar.mileage+
+    ' mileage,with '+
+    aCar.colour.interiorFabric.texture+
+    ' interior.')
